386188e985026fb357013e1af15cb3dc5d3ae98b
DeviatorZ <armandasprogram95@gmail.com>
2023-04-15 19:42:02 +0300

    refactored to use the parent class's _setComponent method for overriding the shooting component

--- WIP/Tanks/AT_SPG.py
+++ WIP/Tanks/AT_SPG.py
@@ -1,6 +1,7 @@
 from TankComponents.DirectShootingComponent import DirectShootingComponent
 from Tanks.Tank import Tank
 from Aliases import positionTuple
+from Aliases import jsonDict
 import Tanks.Settings as Settings
 
 class AT_SPG(Tank):
@@ -9,5 +10,12 @@ class AT_SPG(Tank):
     def __init__(self, spawnPosition: positionTuple, position: positionTuple, currentHealth: int = None) -> None:
         super().__init__(spawnPosition, position, Settings.AT_SPG, currentHealth)
   
-    def _initializeShooting(self):
-        self.components["shooting"] = DirectShootingComponent(Settings.AT_SPG["maxAttackDistance"], Settings.AT_SPG["damage"])
+    def _initializeShooting(self, settings: jsonDict) -> None:
+        """
+        Overrides initialization of the shooting component for the AT-SPG tank.
+
+        :param settings: A dictionary containing the settings of the tank.
+            - "maxAttackDistance": An integer representing the maximum attack distance of the direct shot.
+            - "damage": An integer representing the damage dealt by the tank's attacks.
+        """
+        self._setComponent("shooting", DirectShootingComponent(settings["maxAttackDistance"], settings["damage"]))
