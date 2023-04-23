2ceb8b07a6da3340deffdc5d2ba4c51259c8a093
DeviatorZ <armandasprogram95@gmail.com>
2023-04-23 19:30:07 +0300

    added respawn handling

--- WIP/TankSystems/TankHealthSystem.py
+++ WIP/TankSystems/TankHealthSystem.py
@@ -1,41 +1,37 @@
 from Events.Events import TankAddedEvent
 from Events.Events import TankShotEvent
+from Events.Events import TankDestroyedEvent
+from Events.Events import TankRespawnedEvent
 from Events.EventManager import EventManager
-from Map import Map
 from Tanks.Tank import Tank
 
 class TankHealthSystem:
     """
     A system that manages the health of tanks.
     """
-    def __init__(self, map: Map, eventManager: EventManager):
+    def __init__(self, eventManager: EventManager):
         """
         Initializes the TankHealthSystem.
 
-        :param map: An instance of the Map that holds static game information.
         :param eventManager: The EventManager instance to use for triggering events.
         """
-        self.__map = map
         self.__eventManager = eventManager
         self.__eventManager.addHandler(TankAddedEvent, self.onTankAdded)
         self.__eventManager.addHandler(TankShotEvent, self.onTankShot)
+        self.__eventManager.addHandler(TankRespawnedEvent, self.onTankRespawned)
         self.__tanks = {}
 
     def onTankAdded(self, tankId: str, tankEntity: Tank) -> None:
         """
-        Event handler. Adds the tank to the system if it has a health and position components
+        Event handler. Adds the tank to the system if it has a health component
 
         :param tankId: The ID of the added tank.
         :param tankEntity: The Tank entity that was added.
         """
         healthComponent = tankEntity.getComponent("health")
-        positionComponent = tankEntity.getComponent("position")
 
-        if healthComponent and positionComponent:
-            self.__tanks[tankId] = {
-                "position": positionComponent,
-                "health": healthComponent
-            }
+        if healthComponent:
+            self.__tanks[tankId] = healthComponent
 
     def onTankShot(self, tankId: str, damage: int):
         """
@@ -44,8 +40,21 @@ class TankHealthSystem:
         :param tankId: The ID of the tank that got shot.
         :param damage: Amount of damage.
         """
-        tank = self.__tanks.get(tankId)
+        healthComponent = self.__tanks.get(tankId)
 
-        if tank:
-            healthComponent = tank["health"]
+        if healthComponent:
             healthComponent.currentHealth -= damage
+
+            if healthComponent.currentHealth <= 0:
+                EventManager.triggerEvent(TankDestroyedEvent, tankId)
+
+    def onTankRespawned(self, tankId: str) -> None:
+        """
+        Event handler. Handles healing a tank to full on respawn.
+
+        :param tankId: The ID of the tank that got respawned.
+        """
+        healthComponent = self.__tanks.get(tankId)
+
+        if healthComponent:
+            healthComponent.currentHealth = healthComponent.maxHealth
